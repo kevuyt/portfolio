c0fcecaf2f591b62e5e57b573d6eb9ff43523d27
kevuyt <kevalkataria@gmail.com>
2024-08-15 08:53:30 -0400

    attempt 6

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,13 @@ import Home from './components/Home';
 import About from './components/About';
 import Projects from './components/Projects';
 import Contact from './components/Contact';
+import Navbar from './components/Navbar';
 
 const App = () => {
   return (
     <Router>
       <div>
+      <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
