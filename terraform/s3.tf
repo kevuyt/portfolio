a6538fd83fedacc415cc012f2b2c21785fa79888
kevuyt <kevalkataria@gmail.com>
2024-08-15 08:18:25 -0400

    attempt 5

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -10,6 +10,22 @@ resource "aws_s3_bucket_object" "portfolio_files" {
   acl      = "public-read"
 }
 
+resource "aws_s3_bucket_policy" "frontend_bucket_policy" {
+  bucket = aws_s3_bucket.frontend_bucket.id
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Principal = "*",
+        Action = "s3:GetObject",
+        Resource = "${aws_s3_bucket.frontend_bucket.arn}/*"
+      }
+    ]
+  })
+}
+
 resource "aws_s3_bucket_versioning" "bucket_versioning" {
   bucket = aws_s3_bucket.frontend_bucket.id
   versioning_configuration {
